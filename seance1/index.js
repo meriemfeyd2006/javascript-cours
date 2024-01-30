25485b1481306f046b9de0f5e5f30257cc62cb98
meriemfeyd2006 <156081065+meriemfeyd2006@users.noreply.github.com>
2024-01-30 09:53:38 +0100

    test

---
+++ seance1/index.js
@@ -0,0 +1,11 @@
+// les fonctions en javascript 
+
+function greet(nom){
+    console.log("helloooo!"+nom)
+}
+
+function somme(nombre1, nombre2){
+    return nombre1 + nombre2
+}
+console.log(somme(13,80))
+
---
+++ seance2/index.js
@@ -0,0 +1,27 @@
+
+let x='5';
+let y=5;
+if(x===y){
+   console.log("ok!!!")
+}
+else{
+    console.log("no!!")
+}
+if(x!==y){
+    console.log("oui!!!")
+ }
+ else{
+     console.log("non!!")
+}
+let statut="etudiant";
+let age=20;
+if(statut && age){
+   console.log("yes")
+}else{
+    console.log("now")
+}
+if(statut && age){
+    console.log("yes")
+ }else{
+     console.log("now")
+ }
